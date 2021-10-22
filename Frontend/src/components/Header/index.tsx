46a1cf3bfd838c71f2029cf9ce6034dacfb582c3
kleber <kleberrocha2701@gmail.com>
2021-10-21 22:59:46 -0300

    Wip home

---
+++ Frontend/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+import style from '../../../styles/components/Header.module.css';
+
+export const Header: React.FC = () => {
+  return (
+    <nav className={style.container}>
+      <Link href="/">
+        <a>
+          <img src="/images/Logo.svg" alt="logo" />
+        </a>
+      </Link>
+      <Link href="/signin">
+      <a>
+      <button type="button" className="btn-signin">
+        SignIn
+      </button>
+      </a>
+      </Link>
+    </nav>
+  );
+};
