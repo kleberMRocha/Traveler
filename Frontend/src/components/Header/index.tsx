72de2ed90ed5dc87b90aeb546f6d14ea8420929c
kleber <kleberrocha2701@gmail.com>
2021-10-23 16:15:46 -0300

    Ajuste Header ocultar elementos

--- Frontend/src/components/Header/index.tsx
+++ Frontend/src/components/Header/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import Link from 'next/link';
 import style from '../../../styles/components/Header.module.css';
+import {FiSearch} from 'react-icons/fi';
+import { useRouter } from 'next/router'
 
 export const Header: React.FC = () => {
+
+  const router = useRouter();
+  const isHome = useMemo(() => router.pathname === '/',[router]);
+  const getStyleBorder = useCallback(() => {
+    return !isHome ? `${style.borderGray}` : '';
+  },[isHome]);
+
   return (
-    <nav className={style.container}>
+    <nav className={`${style.container} ${getStyleBorder()}`}>
       <Link href="/">
         <a>
           <img src="/images/Logo.svg" alt="logo" />
         </a>
       </Link>
+
+      {
+        !isHome &&  (<span className={style.inputSearch}>
+          <FiSearch /> <input type="text" placeholder="Qual cidade você procura?" />
+        </span>)
+      }
+    
       <Link href="/signin">
       <a>
       <button type="button" className="btn-signin">
