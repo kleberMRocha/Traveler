41bd94ab0f0036685d3ca23a38802354fda6f75b
kleber <kleberrocha2701@gmail.com>
2021-12-13 22:17:35 -0300

    excluir um lugar

--- src/components/shared/table.tsx
+++ src/components/shared/table.tsx
@@ -3,6 +3,10 @@ import style from '../../../styles/components/Table.module.css';
 import { FiCheck, FiX, FiEdit,  FiTrash } from 'react-icons/fi';
 import { format } from 'date-fns';
 import InputSearch from './InputSearch';
+import api from '../../services/axios';
+import { toast } from 'react-toastify';
+import LoaderPage from './LoaderPage';
+
 
 interface ITableInfos {
   type: 'places' | 'attacttion' | 'review';
@@ -15,7 +19,32 @@ const TableInfos: React.FC<ITableInfos> = ({ type, info }) => {
     setInfo(info);
   },[info]);
 
+  interface IDeletePlace{
+    place_name: string,
+    id: string
+  }
+
   const [infoTable,setInfo] = useState(info);
+  const [confirmDelete, setDeleteConfirm] = useState<IDeletePlace>({} as IDeletePlace);
+  const  [isLoading, setIsLoading] = useState(false);
+
+  const handleDeletePlaces = async (id:string) => {
+     setIsLoading(true);
+
+      try {
+       await api.delete(`/places/${id}`);
+       const newTable = infoTable.filter( p => p.id !== id);
+       setInfo(newTable);
+       toast.success('Lugar excluido com sucesso');
+       setDeleteConfirm({} as IDeletePlace);
+       setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        toast.error('Houve um erro ao tentar exlcuir');
+        setIsLoading(false);
+      }
+      
+  };
 
   const headers = {
     places: [
@@ -33,6 +62,21 @@ const TableInfos: React.FC<ITableInfos> = ({ type, info }) => {
   return (
     <>
     <InputSearch info={info} handleFilterInfo={setInfo}  />
+    
+    {
+     isLoading ? <LoaderPage /> :
+      confirmDelete.place_name &&
+      <div className={style.confirmDelete}>
+      Certeza que deseja excluir <u>{confirmDelete.place_name}?</u>
+      <b>Essa ação será permanente</b>
+      <button
+      onClick={() => handleDeletePlaces(confirmDelete.id)}
+      >Confirmar</button>
+      </div>
+
+    }
+   
+
     <table className={style.tableinfo}>
       <thead>
         <tr>
@@ -62,7 +106,7 @@ const TableInfos: React.FC<ITableInfos> = ({ type, info }) => {
                   </button>
                 </td>
                 <td>
-                  <button>
+                  <button onClick={() => setDeleteConfirm(td)}>
                     <FiTrash color="tomato" />
                   </button>
                 </td>
