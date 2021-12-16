8c0b19fd019556e212e95038110f81445d814422
kleber <kleberrocha2701@gmail.com>
2021-12-15 23:04:08 -0300

    Editar um lugar

--- src/components/shared/table.tsx
+++ src/components/shared/table.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import style from '../../../styles/components/Table.module.css';
-import { FiCheck, FiX, FiEdit,  FiTrash } from 'react-icons/fi';
+import { FiCheck, FiX, FiEdit, FiTrash, FiCheckCircle, FiArrowLeft } from 'react-icons/fi';
 import { format } from 'date-fns';
 import InputSearch from './InputSearch';
 import api from '../../services/axios';
 import { toast } from 'react-toastify';
 import LoaderPage from './LoaderPage';
 import { useModal } from '../../context/useModal';
-
+import { cloneObj } from '../../utils/cloneObj';
 
 
 interface ITableInfos {
@@ -16,38 +16,99 @@ interface ITableInfos {
 }
 
 const TableInfos: React.FC<ITableInfos> = ({ type, info }) => {
+  interface IDeletePlace {
+    place_name: string;
+    id: string;
+  }
 
-  const {handleOpenModal} = useModal();
+  interface IPlace{
+    id: string;
+    img_url: string;
+    place_name: string;
+    place_desc: string;
+  }
+
+  const { handleOpenModal } = useModal();
 
   useEffect(() => {
     setInfo(info);
-  },[info]);
+  }, [info]);
 
-  interface IDeletePlace{
-    place_name: string,
-    id: string
-  }
 
-  const [infoTable,setInfo] = useState(info);
-  const [confirmDelete, setDeleteConfirm] = useState<IDeletePlace>({} as IDeletePlace);
-  const  [isLoading, setIsLoading] = useState(false);
+  const [infoTable, setInfo] = useState(info);
+  const [confirmDelete, setDeleteConfirm] = useState<IDeletePlace>(
+    {} as IDeletePlace
+  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEditar, setIsEditar] = useState(false);
 
-  const handleDeletePlaces = async (id:string) => {
-     setIsLoading(true);
+  const [placeTobeEdited, setEditPlace] = useState({} as IPlace);
+
+  const handleChangeEditPalce = ( value: string,key:'place_name' | 'place_desc') => {
+    const obj:IPlace = cloneObj(placeTobeEdited);
+    obj[key] = value;
+    setEditPlace(obj);
+  };
+
+  const handleEditPlace = async (action:boolean, value?:IPlace) => {
+
+    if(!action && !value){
+      setInfo(info);
+      setIsEditar(action);
+      return;
+    }
+
+
+    if(value && action){
+
+      setInfo([value]);
+      setEditPlace(value);
+
+    }else{
+      const {id,place_desc,place_name} = placeTobeEdited;
+
+      const dataEditPlace = {place_name, place_desc };
 
       try {
-       await api.delete(`/places/${id}`);
-       const newTable = infoTable.filter( p => p.id !== id);
-       setInfo(newTable);
-       toast.success('Lugar excluido com sucesso');
-       setDeleteConfirm({} as IDeletePlace);
-       setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-        toast.error('Houve um erro ao tentar exlcuir');
+        setIsLoading(true);
+        await api.put(`places/${id}`, dataEditPlace);
+        toast.success('Informações Atualizadas com sucesso!');
+        const newInfo = info.map((i:IPlace) => {
+          if(i.id === id){
+            i.place_name = place_name;
+            i.place_desc = place_desc;
+          }
+          return i;
+        });
+        setInfo(newInfo);
         setIsLoading(false);
+      } catch (error) {
+        toast.success('Houve um erro no processo');
+        console.log(error);
+        setIsLoading(false); 
       }
-      
+
+   
+    }
+
+    setIsEditar(action);
+  };
+
+  const handleDeletePlaces = async (id: string) => {
+    setIsLoading(true);
+
+    try {
+      await api.delete(`/places/${id}`);
+      const newTable = infoTable.filter((p) => p.id !== id);
+      setInfo(newTable);
+      toast.success('Lugar excluido com sucesso');
+      setDeleteConfirm({} as IDeletePlace);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      toast.error('Houve um erro ao tentar exlcuir');
+      setIsLoading(false);
+    }
   };
 
   const headers = {
@@ -65,43 +126,67 @@ const TableInfos: React.FC<ITableInfos> = ({ type, info }) => {
 
   return (
     <>
-    <InputSearch info={info} handleFilterInfo={setInfo}  />
+      <InputSearch info={info} handleFilterInfo={setInfo} />
 
-    {
-     isLoading ? <LoaderPage /> :
-      confirmDelete.place_name &&
-      <div className={style.confirmDelete}>
-      Certeza que deseja excluir <u>{confirmDelete.place_name}?</u>
-      <b>Essa ação será permanente</b>
-      <button
-      onClick={() => handleDeletePlaces(confirmDelete.id)}
-      >Confirmar</button>
-      </div>
+      {isLoading ? (
+        <LoaderPage />
+      ) : (
+        confirmDelete.place_name && (
+          <div className={style.confirmDelete}>
+            Certeza que deseja excluir <u>{confirmDelete.place_name}?</u>
+            <b>Essa ação será permanente</b>
+            <button onClick={() => handleDeletePlaces(confirmDelete.id)}>
+              Confirmar
+            </button>
+            <button 
+            onClick={() => setDeleteConfirm({} as IDeletePlace)}>
+              Cancelar
+            </button>
+          </div>
+        )
+      )}
 
-    }
-   
-
-    <table className={style.tableinfo}>
-      <thead>
-        <tr>
-          {headers[type].map((th) => (
-            <th key={th}>{th}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-          {!infoTable.length && <tr><td colSpan={8}> Nada encontrado </td></tr>}
-        {infoTable.map((td) => {
-          return (
+      <table className={style.tableinfo}>
+        <thead>
+          <tr>
+            {headers[type].map((th) => (
+              <th key={th}>{th}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {!infoTable.length && (
+            <tr>
+              <td colSpan={8}> Nada encontrado </td>
+            </tr>
+          )}
+          {infoTable.map((td) => {
+            return (
               <tr key={td.id}>
-                <td>{td.place_name}</td>
-                <td>{td.place_desc.substring(0,50)} ... </td>
-                <td style={{cursor:'not-allowed'}}>
+                <td>
+                  {isEditar ? (
+                    <input type="text" onChange={(e) => {
+                      handleChangeEditPalce(e.target.value,'place_name');
+                    }} value={placeTobeEdited.place_name} />
+                  ) : (
+                    td.place_name
+                  )}
+                </td>
+                <td>
+                  {isEditar ? (
+                    <textarea  onChange={(e) => {
+                      handleChangeEditPalce(e.target.value,'place_desc');
+                    }} value={placeTobeEdited.place_desc} />
+                  ) : (
+                    `${td.place_desc.substring(0, 50)} ...`
+                  )}
+                </td>
+                <td style={{ cursor: 'not-allowed' }}>
                   {td.img_url.length ? (
-                    <button 
+                    <button
                       type="button"
-                      onClick={() =>  handleOpenModal(true, true, td.img_url )}
-                     >
+                      onClick={() => handleOpenModal(true, true, td.img_url)}
+                    >
                       <FiCheck color="green" />
                     </button>
                   ) : (
@@ -110,20 +195,37 @@ const TableInfos: React.FC<ITableInfos> = ({ type, info }) => {
                 </td>
                 <td>{format(new Date(td.updated_at), 'dd/MM/yyyy')}</td>
                 <td>
-                  <button>
-                    <FiEdit color="#bb7c08" />
-                  </button>
+                  {!isEditar ? (
+                    <button onClick={() => handleEditPlace(true, td )}>
+                      <FiEdit color="#bb7c08" />
+                    </button>
+                  ) : (
+                   <>
+                    <button  
+                      className={style.confirmEdit} 
+                      onClick={() => handleEditPlace(false, td )} 
+                    >
+                      <FiCheckCircle color="green" /> Confirmar
+                    </button>
+                    <button  
+                      className={style.goBack} 
+                      onClick={() => handleEditPlace(false)} 
+                    >
+                      <FiArrowLeft color="gray" /> Voltar
+                    </button>
+                   </>
+                  )}
                 </td>
                 <td>
-                  <button onClick={() => setDeleteConfirm(td)}>
+                  {!isEditar && <button onClick={() => setDeleteConfirm(td)}>
                     <FiTrash color="tomato" />
-                  </button>
+                  </button>}
                 </td>
               </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 };
